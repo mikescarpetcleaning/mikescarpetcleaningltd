ac7444942607abd9ca0e03d39feb94e2dbe98ed6
Geoff Jarman <geoffxp@gmail.com>
2022-04-24 21:02:16 -0500

    adding drop downs

--- pages/estimater.tsx
+++ pages/estimater.tsx
@@ -3,9 +3,18 @@ import { useEffect, useState } from 'react';
 import styles from "../estimaterStyles/Estimater.module.css";
 
 type Estimate = {
+    areas: number,
+    pet: number,
+    scotch: number,
     rooms: number,
     petRooms: number,
     scotchRooms: number,
+    halls: number,
+    petHalls: number,
+    scotchHalls: number,
+    stairs: number,
+    petStairs: number,
+    scotchStairs: number,
     linearFeet: number,
     chairs: number,
     seatCushions: number,
@@ -21,9 +30,18 @@ type Estimate = {
 
 const Estimater: NextPage = () => {
     const defaultEstimate: Estimate = {
+        areas: 0,
+        pet: 0,
+        scotch: 0,
         rooms: 0,
         petRooms: 0,
         scotchRooms: 0,
+        halls: 0,
+        petHalls: 0,
+        scotchHalls: 0,
+        stairs: 0,
+        petStairs: 0,
+        scotchStairs: 0,
         linearFeet: 0,
         chairs: 0,
         seatCushions: 0,
@@ -37,9 +55,9 @@ const Estimater: NextPage = () => {
         dryerVents: 0
     }
     const rates = {
-        rooms: 40,
-        petRooms: 20,
-        scotchRooms: 15,
+        areas: 40,
+        pet: 20,
+        scotch: 15,
         linearFeet: 19,
         chairs: 69,
         seatCushions: 5,
@@ -79,8 +97,40 @@ const Estimater: NextPage = () => {
             })
         }
     }
+    const toggleDropdown = ({ target }: any) => {
+        const group = target.nextElementSibling;
+        if (group.style.maxHeight) {
+            group.style.maxHeight = '';
+            target.classList.remove('open')
+        } else {
+            group.style.maxHeight = group.scrollHeight + 'px';
+            target.classList.add('open')
+        }
+    }
     useEffect(() => {
-        if (estimate.rooms >= estimate.petRooms && estimate.rooms >= estimate.scotchRooms) {
+        if ((estimate.rooms + estimate.halls + estimate.stairs) !== estimate.areas) {
+            setEstimate({
+                ...estimate,
+                areas: estimate.rooms + estimate.halls + estimate.stairs
+            })
+        }
+        if ((estimate.petRooms + estimate.petHalls + estimate.petStairs) !== estimate.pet) {
+            setEstimate({
+                ...estimate,
+                pet: estimate.petRooms + estimate.petHalls + estimate.petStairs
+            })
+        }
+        if ((estimate.scotchRooms + estimate.scotchHalls + estimate.scotchStairs) !== estimate.scotch) {
+            setEstimate({
+                ...estimate,
+                scotch: estimate.scotchRooms + estimate.scotchHalls + estimate.scotchStairs
+            })
+        }
+        if (
+            (estimate.rooms >= estimate.petRooms && estimate.rooms >= estimate.scotchRooms) &&
+            (estimate.halls >= estimate.petHalls && estimate.halls >= estimate.scotchHalls) &&
+            (estimate.stairs >= estimate.petStairs && estimate.stairs >= estimate.scotchStairs)
+            ) {
             console.log("before:", estimate.rooms, estimate.petRooms)
             const rateArray = Object.entries(rates);
             const total = rateArray.reduce((acc: number, rate: [string, number]): number => {
@@ -89,7 +139,7 @@ const Estimater: NextPage = () => {
                 let current = estimate[rate[0] as keyof Estimate];
                 if (current < 0) current = 0;
     
-                if (rate[0] === 'rooms') {
+                if (rate[0] === 'areas') {
                     if (current > 0 && current < 5) {
                         minimum = 149;
                         current = current - 2 > 0 ? current - 2 : 0;
@@ -101,14 +151,14 @@ const Estimater: NextPage = () => {
                     }
                 }
     
-                if (rate[0] === 'petRooms') {
+                if (rate[0] === 'pet') {
                     if (current > 0 && current < 2) {
                         minimum = 40;
                         current = 0;
                     }
                 }
     
-                if (rate[0] === 'scotchRooms') {
+                if (rate[0] === 'scotch') {
                     if (current > 0 && current < 2) {
                         minimum = 30;
                         current = 0;
@@ -122,7 +172,15 @@ const Estimater: NextPage = () => {
             setEstimate({...estimate, 'petRooms': estimate.rooms})
         } else if (estimate.rooms < estimate.scotchRooms) {
             setEstimate({...estimate, 'scotchRooms': estimate.rooms})
-        }
+        } else if (estimate.halls < estimate.petHalls) {
+            setEstimate({...estimate, 'petHalls': estimate.halls})
+        } else if (estimate.halls < estimate.scotchHalls) {
+            setEstimate({...estimate, 'scotchHalls': estimate.halls})
+        } else if (estimate.stairs < estimate.petStairs) {
+            setEstimate({...estimate, 'petStairs': estimate.stairs})
+        } else if (estimate.stairs < estimate.scotchStairs) {
+            setEstimate({...estimate, 'scotchStairs': estimate.stairs})
+        } 
     }, [estimate])
 
     useEffect(() => {
@@ -136,54 +194,166 @@ const Estimater: NextPage = () => {
             <form>
                 <h2>Carpets</h2>
                 <div className={styles.formRow}>
-                    <h3>ROOMS / AREAS</h3>
-                    <div className={styles.formItem}>
-                        <label htmlFor="rooms">Clean</label>
-                        <div className={styles.switcher}>
-                            <button type="button" onClick={decreaseEstimate}>-</button>
-                            <input 
-                                id="rooms" 
-                                name="rooms"
-                                type="text"
-                                value={estimate.rooms}
-                                onChange={updateEstimate} 
-                            />
-                            <button type="button" onClick={incrementEstimate}>+</button>
+                    <h3 onClick={toggleDropdown}>ROOMS</h3>
+                    <div className={styles.formGroup}>
+                        <div className={styles.formItem}>
+                            <label htmlFor="rooms">Clean</label>
+                            <div className={styles.switcher}>
+                                <button type="button" onClick={decreaseEstimate}>-</button>
+                                <input 
+                                    id="rooms" 
+                                    name="rooms"
+                                    type="text"
+                                    value={estimate.rooms}
+                                    onChange={updateEstimate} 
+                                />
+                                <button type="button" onClick={incrementEstimate}>+</button>
+                            </div>
                         </div>
-                    </div>
-                    <div className={styles.formItem}>
-                        <label htmlFor="petRooms">Pet Treat</label>
-                        <div className={styles.switcher}>
-                            <button type="button" onClick={decreaseEstimate} disabled={allPet ? true : false}>-</button>
-                            <input 
-                                id="petRooms" 
-                                name="petRooms"
-                                type="text"
-                                value={estimate.petRooms}
-                                onChange={updateEstimate} 
-                            />
-                            <button type="button" onClick={incrementEstimate} disabled={allPet ? true : false}>+</button>
-                            <div className={styles.allBox}>
-                                <label htmlFor="allPet">all?</label>
-                                <input type="checkbox" id="allPet" onChange={() => setAllPet(!allPet)} />
+                        <div className={styles.formItem}>
+                            <label htmlFor="petRooms">Pet Treat</label>
+                            <div className={styles.switcher}>
+                                <button type="button" onClick={decreaseEstimate} disabled={allPet ? true : false}>-</button>
+                                <input 
+                                    id="petRooms" 
+                                    name="petRooms"
+                                    type="text"
+                                    value={estimate.petRooms}
+                                    onChange={updateEstimate} 
+                                />
+                                <button type="button" onClick={incrementEstimate} disabled={allPet ? true : false}>+</button>
+                                <div className={styles.allBox}>
+                                    <label htmlFor="allPet">all?</label>
+                                    <input type="checkbox" id="allPet" onChange={() => setAllPet(!allPet)} />
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles.formItem}>
+                            <label htmlFor="scotchRooms">ScotchGard</label>
+                            <div className={styles.switcher}>
+                                <button type="button" onClick={decreaseEstimate} disabled={allScotch ? true : false}>-</button>
+                                <input 
+                                    id="scotchRooms" 
+                                    name="scotchRooms"
+                                    type="text"
+                                    value={estimate.scotchRooms}
+                                    onChange={updateEstimate} 
+                                />
+                                <button type="button" onClick={incrementEstimate} disabled={allScotch ? true : false}>+</button>
+                                <div className={styles.allBox}>
+                                    <label htmlFor="allScotch">all?</label>
+                                    <input type="checkbox" id="allScotch" onChange={() => setAllScotch(!allScotch)} />
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div className={styles.formItem}>
-                        <label htmlFor="scotchRooms">ScotchGard</label>
-                        <div className={styles.switcher}>
-                            <button type="button" onClick={decreaseEstimate} disabled={allScotch ? true : false}>-</button>
-                            <input 
-                                id="scotchRooms" 
-                                name="scotchRooms"
-                                type="text"
-                                value={estimate.scotchRooms}
-                                onChange={updateEstimate} 
-                            />
-                            <button type="button" onClick={incrementEstimate} disabled={allScotch ? true : false}>+</button>
-                            <div className={styles.allBox}>
-                                <label htmlFor="allScotch">all?</label>
-                                <input type="checkbox" id="allScotch" onChange={() => setAllScotch(!allScotch)} />
+                </div>
+                <div className={styles.formRow}>
+                    <h3 onClick={toggleDropdown}>STAIRCASES</h3>
+                    <div className={styles.formGroup}>
+                        <div className={styles.formItem}>
+                            <label htmlFor="halls">Clean</label>
+                            <div className={styles.switcher}>
+                                <button type="button" onClick={decreaseEstimate}>-</button>
+                                <input 
+                                    id="halls" 
+                                    name="halls"
+                                    type="text"
+                                    value={estimate.halls}
+                                    onChange={updateEstimate} 
+                                />
+                                <button type="button" onClick={incrementEstimate}>+</button>
+                            </div>
+                        </div>
+                        <div className={styles.formItem}>
+                            <label htmlFor="petHalls">Pet Treat</label>
+                            <div className={styles.switcher}>
+                                <button type="button" onClick={decreaseEstimate} disabled={allPet ? true : false}>-</button>
+                                <input 
+                                    id="petHalls" 
+                                    name="petHalls"
+                                    type="text"
+                                    value={estimate.petHalls}
+                                    onChange={updateEstimate} 
+                                />
+                                <button type="button" onClick={incrementEstimate} disabled={allPet ? true : false}>+</button>
+                                <div className={styles.allBox}>
+                                    <label htmlFor="allPet">all?</label>
+                                    <input type="checkbox" id="allPet" onChange={() => setAllPet(!allPet)} />
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles.formItem}>
+                            <label htmlFor="scotchHalls">ScotchGard</label>
+                            <div className={styles.switcher}>
+                                <button type="button" onClick={decreaseEstimate} disabled={allScotch ? true : false}>-</button>
+                                <input 
+                                    id="scotchHalls" 
+                                    name="scotchHalls"
+                                    type="text"
+                                    value={estimate.scotchHalls}
+                                    onChange={updateEstimate} 
+                                />
+                                <button type="button" onClick={incrementEstimate} disabled={allScotch ? true : false}>+</button>
+                                <div className={styles.allBox}>
+                                    <label htmlFor="allScotch">all?</label>
+                                    <input type="checkbox" id="allScotch" onChange={() => setAllScotch(!allScotch)} />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.formRow}>
+                    <h3 onClick={toggleDropdown}>HALLWAYS</h3>
+                    <div className={styles.formGroup}>
+                        <div className={styles.formItem}>
+                            <label htmlFor="stairs">Clean</label>
+                            <div className={styles.switcher}>
+                                <button type="button" onClick={decreaseEstimate}>-</button>
+                                <input 
+                                    id="stairs" 
+                                    name="stairs"
+                                    type="text"
+                                    value={estimate.stairs}
+                                    onChange={updateEstimate} 
+                                />
+                                <button type="button" onClick={incrementEstimate}>+</button>
+                            </div>
+                        </div>
+                        <div className={styles.formItem}>
+                            <label htmlFor="petStairs">Pet Treat</label>
+                            <div className={styles.switcher}>
+                                <button type="button" onClick={decreaseEstimate} disabled={allPet ? true : false}>-</button>
+                                <input 
+                                    id="petStairs" 
+                                    name="petStairs"
+                                    type="text"
+                                    value={estimate.petStairs}
+                                    onChange={updateEstimate} 
+                                />
+                                <button type="button" onClick={incrementEstimate} disabled={allPet ? true : false}>+</button>
+                                <div className={styles.allBox}>
+                                    <label htmlFor="allPet">all?</label>
+                                    <input type="checkbox" id="allPet" onChange={() => setAllPet(!allPet)} />
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles.formItem}>
+                            <label htmlFor="scotchStairs">ScotchGard</label>
+                            <div className={styles.switcher}>
+                                <button type="button" onClick={decreaseEstimate} disabled={allScotch ? true : false}>-</button>
+                                <input 
+                                    id="scotchStairs" 
+                                    name="scotchStairs"
+                                    type="text"
+                                    value={estimate.scotchStairs}
+                                    onChange={updateEstimate} 
+                                />
+                                <button type="button" onClick={incrementEstimate} disabled={allScotch ? true : false}>+</button>
+                                <div className={styles.allBox}>
+                                    <label htmlFor="allScotch">all?</label>
+                                    <input type="checkbox" id="allScotch" onChange={() => setAllScotch(!allScotch)} />
+                                </div>
                             </div>
                         </div>
                     </div>
