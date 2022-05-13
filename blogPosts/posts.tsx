eaac84ea551df276e70d78f3d97d7d14eee37716
Geoff Jarman <geoffxp@gmail.com>
2022-05-13 10:16:41 -0500

    content and styling for first blog post

--- blogPosts/posts.tsx
+++ blogPosts/posts.tsx
@@ -1,10 +1,115 @@
-
 export const posts: any[] = [
   {
     title: "5 Reasons Why You Should Clean Your Carpets TODAY",
     content: `
-        <p>Maintaining carpet is the responsibility of homeowners that is most often neglected. While it&apos;s easy to see that the grass is getting out of line, or that dishes are piling up a little too high, it may not always be obvious that your carpets are in need of a refresh. This can be detrimental to the lifespan of your carpets, as dirt and debris can scratch the delicate fibers, causing a worn out look prematurely. Imagine if you were to wear the same pair of clothes for years and years on end, your carpet is very similar in how it holds on to dirt and germs. While it doesn&apos;t need quite as much attention as your clothes, your carpet DOES need cleaning on a regular basis. Here are three reasons you should clean your carpet yearly.</p>
-        
+        <img src="/post-1-dirty.webp" alt="dirty carpets with a clean stripe" width="auto" height="auto" />
+        <p>Maintaining carpet is the responsibility of homeowners that is most often neglected. 
+        While it&apos;s easy to see that the grass is getting out of line, or that dishes are 
+        piling up a little too high, it may not always be obvious that your carpets are in need 
+        of a refresh. This can be detrimental to the lifespan of your carpets, as dirt and 
+        debris can scratch the delicate fibers, causing a worn out look prematurely. Imagine 
+        if you were to wear the same pair of clothes for years and years on end, your carpet 
+        is very similar in how it holds on to dirt and germs. While it doesn&apos;t need quite 
+        as much attention as your clothes, your carpet DOES need cleaning on a regular basis. 
+        Here are three reasons you should clean your carpet yearly.</p>
+        <img src="/covid.webp" alt="microscopic rendering of germs" width="auto" height="auto" />
+        <h2>1. Germs and Allergens</h2>
+        <p>E. coli, salmonella, dust mites, and mold are commonly found in uncleaned carpets. 
+        There are over 40,000 reported cases of illness caused by salmonella each year. Norovirus, 
+        the virus that causes the stomach flu, is reported to be able to survive in carpet for 
+        an entire month. Not to mention, carpets are like magnets that collect pollen and dander 
+        and hold onto them tightly. Regular vacuums struggle to remove these tiny particles and 
+        old vacuums may only be aiding in pushing them into the air.</p>
+        <p>That being said, we are not in the business of fear mongering. It would likely be 
+        impossible to eradicate these bacteria completely from a home, but you can keep the 
+        concentration at a safe level with regular maintenance. Six to twelve month cleaning 
+        intervals have been found to be perfectly sufficient for keeping your home healthy and 
+        happy!</p>
+        <img src="/post-1-cleaning-1.webp" alt="cleaning carpets with truckmount system" width="auto" height="auto" />
+        <h2>2. Premature Wear and Tear</h2>
+        <p>If you've been inside many homes, it's likely that you have seen dark pathways running 
+        through the hallways of some carpeted areas. This can give the impression that the carpet 
+        is in need of a bath. However, when carpet fibers approach the end of their useful 
+        lifespan, they simply do not have the reflective and transparent qualities that they used 
+        to. This is due to years and years of scratching and scraping against tiny particles. The 
+        scratched up and split fibers don't stand up properly anymore and tell you that the carpet 
+        needs to be replaced.</p>
+        <p>Most carpets should expect ten to twenty years of useful life but if they are neglected 
+        then that span can be drastically reduced. With a high volume of dirt and debris the damage 
+        to the fibers occurs at a faster rate. Removing these alien particles effectively and 
+        thoroughly is a sure-fire way to safe you money and time. Protect your investment with 
+        regular cleanings!</p>
+        <img src="/post-1-cleaning-2.webp" alt="steam coming out of carpet cleaning machine" width="auto" height="auto" />
+        <h2>3. Enhanced Look And Feel</h2>
+        <p>Let's face it, dirty carpets are not appealing. The way your living areas are perceived 
+        can be degraded by a lack of maintenance, which can lead to a lower quality of life. Not 
+        only that, carpets that are dirty or improperly cleaned can feel stiff, crunchy, or sticky.</p>
+        <p>Trained professionals know exactly how to treat any of these issues. The chemistry of 
+        carpets is actually a complex and delicate balance which can easily be thrown off. Regular 
+        vacuuming and yearly professional cleaning is a NECESSITY for preserving carpets. You spend 
+        a lot of time inside of your home, especially now that a lot of people are also working 
+        from home, So why not elevate your quality of life to the maximum?</p>
+        <a href="https://mikescarpetcleaningltd.com/schedule-carpet-cleaning">Schedule your appointment 
+        online 24/7</a>
+        <style>
+          main {
+           
+            margin: 0 auto;
+          }
+          h1 {
+            font-size: 48px;
+            line-height: 48px;
+            text-transform: uppercase;
+            text-align: center;
+            background: var(--dark);
+            color: white;
+            padding: 48px 25%;
+            margin: 0;
+          }
+          main > img:first-of-type {
+            width: 100%;
+            max-width: none;
+          }
+          main img {
+            width: 100%;
+            max-width: 700px;
+            margin: 0 auto;
+            display: block;
+          }
+          main p {
+            font-size: 20px;
+            font-weight: 200;
+            max-width: 700px;
+            margin: 64px auto;
+            padding: 0 16px
+          }
+          main h2 {
+            margin-top: 48px;
+          }
+          main a {
+            margin: 24px auto 64px;
+            display: block;
+            width: fit-content;
+            font-size: 24px;
+            text-decoration: underline;
+          }
+          @media screen and (max-width: 820px) {
+            h1 {
+              padding: 32px 16px;
+              font-size: 32px;
+              line-height: 32px;
+            }
+            main p {
+              margin: 32px auto;
+            }
+            main a {
+              font-size: 20px;
+              text-align: center;
+              margin: 0 auto 64px;
+              max-width: 288px;
+            }
+          }
+        </style>
         `,
   },
 ];
