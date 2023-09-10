4314cd92d6fa9593e7d9b18136d4776076ebf80b
geoffxp <geoffxp@gmail.com>
2023-09-10 15:41:06 -0700

    adding another blog post

--- blogPosts/posts.tsx
+++ blogPosts/posts.tsx
@@ -114,4 +114,190 @@ export const posts: any[] = [
         </style>
         `,
   },
+  {
+    title: "Unlocking the Beauty of Your Tiles: The Advantages of Professional Tile Cleaning Services",
+    featured_image: `<img src="/green-tile-very-clean.jpg" alt="Very clean green tiles" width="auto" height="auto" />`,
+    excerpt: "Are your tile floors losing their luster, plagued by stubborn stains and grime that seem impossible to remove? It's a common frustration for many homeowners. However, there's a solution that goes beyond DIY efforts: professional tile cleaning services.",
+    content: `
+    <img src="/green-tile-very-clean.jpg" alt="very clean green tiles" width="auto" height="auto" />
+    <div style="padding: 2.5%">
+        <!-- Title Section -->
+        <h1 class="text-3xl md:text-4xl mb-8 font-bold">The Benefits of Professional Tile Cleaning Services</h1>
+        <div style="max-width: 600px; margin: 0 auto;">
+
+        <!-- Introduction Section -->
+        <section class="mb-8">
+            <h2 class="text-2xl font-semibold mb-4">Introduction</h2>
+            <p>
+                Are you tired of struggling with DIY tile cleaning methods that often leave your floors less than perfect? It's time to discover the remarkable advantages of turning to professional tile cleaning services. In this comprehensive guide, we'll explore why entrusting your tile cleaning needs to experts can make a world of difference in the appearance and longevity of your tiles.
+            </p>
+        </section>
+        <img src="/clean-kitchen-tile.jpg" alt="some very clean kitchen tiles" width="auto" height="auto" />
+
+        <!-- Chapter 1: Expertise and Experience -->
+        <section class="mb-8">
+            <h2 class="text-2xl font-semibold mb-4">Chapter 1: Expertise and Experience</h2>
+            
+            <!-- Subsection 1.1 -->
+            <div class="mb-4">
+                <h3 class="text-xl font-semibold mb-2">1.1 Trained Professionals</h3>
+                <p>
+                    Professional tile cleaning companies employ trained experts who understand the intricacies of different tile types and the appropriate cleaning methods. Their expertise ensures that your tiles are treated with care and cleaned effectively.
+                </p>
+            </div>
+            
+            <!-- Subsection 1.2 -->
+            <div>
+                <h3 class="text-xl font-semibold mb-2">1.2 Years of Experience</h3>
+                <p>
+                    With years of experience, professional tile cleaners have encountered various tile cleaning challenges. They have honed their skills and techniques to tackle even the most stubborn stains and grime, leaving your tiles looking pristine.
+                </p>
+            </div>
+        </section>
+        <img src="/pink-tile-very-clean.jpg" alt="pretty pink tile" width="auto" height="auto" />
+        <!-- Chapter 2: Specialized Equipment -->
+        <section class="mb-8">
+            <h2 class="text-2xl font-semibold mb-4">Chapter 2: Specialized Equipment</h2>
+            
+            <!-- Subsection 2.1 -->
+            <div class="mb-4">
+                <h3 class="text-xl font-semibold mb-2">2.1 High-Quality Tools</h3>
+                <p>
+                    Professional tile cleaning companies invest in state-of-the-art equipment designed specifically for tile cleaning. These tools are more powerful and efficient than consumer-grade options, ensuring a thorough and deep clean.
+                </p>
+            </div>
+            
+            <!-- Subsection 2.2 -->
+            <div class="mb-4">
+                <h3 class="text-xl font-semibold mb-2">2.2 Safe and Effective Chemicals</h3>
+                <p>
+                    Tile cleaning experts use safe and effective cleaning solutions that are tough on grime but gentle on your tiles. This means your tiles are cleaned without the risk of damage from harsh chemicals.
+                </p>
+            </div>
+
+            <!-- Subsection 2.3 -->
+            <div>
+                <h3 class="text-xl font-semibold mb-2">2.3 Restoration Capabilities</h3>
+                <p>
+                    Professional tile cleaners can restore the original beauty of your tiles. They have the tools and knowledge to address issues like grout discoloration and tile surface damage, enhancing the overall appearance of your floors.
+                </p>
+            </div>
+        </section>
+        <img src="/outdoor-tile.jpg" alt="Outdoor tile" width="auto" height="auto" />
+
+        <!-- Chapter 3: Time and Effort Savings -->
+        <section class="mb-8">
+            <h2 class="text-2xl font-semibold mb-4">Chapter 3: Time and Effort Savings</h2>
+
+            <!-- Subsection 3.1 -->
+            <div class="mb-4">
+                <h3 class="text-xl font-semibold mb-2">3.1 Leave It to the Pros</h3>
+                <p>
+                    By hiring professionals, you can reclaim your precious time and avoid the laborious task of tile cleaning. Let the experts handle the job while you focus on more important matters.
+                </p>
+            </div>
+
+            <!-- Subsection 3.2 -->
+            <div>
+                <h3 class="text-xl font-semibold mb-2">3.2 Consistent Results</h3>
+                <p>
+                    Professional tile cleaning companies deliver consistent, high-quality results every time. Say goodbye to the frustration of uneven or incomplete DIY cleaning efforts.
+                </p>
+            </div>
+        </section>
+
+        <!-- Chapter 4: Health and Safety -->
+        <section class="mb-8">
+            <h2 class="text-2xl font-semibold mb-4">Chapter 4: Health and Safety</h2>
+
+            <!-- Subsection 4.1 -->
+            <div class="mb-4">
+                <h3 class="text-xl font-semibold mb-2">4.1 Allergen Removal</h3>
+                <p>
+                    Professional tile cleaning services help remove allergens, mold, and bacteria from your tiles. This promotes a healthier indoor environment, especially important for those with allergies or respiratory issues.
+                </p>
+            </div>
+
+            <!-- Subsection 4.2 -->
+            <div>
+                <h3 class="text-xl font-semibold mb-2">4.2 Reduced Risk of Accidents</h3>
+                <p>
+                    Properly cleaned and maintained tiles are less slippery, reducing the risk of slip-and-fall accidents. Professional tile cleaning enhances safety in your home or business.
+                </p>
+            </div>
+        </section>
+
+        <!-- Chapter 5: Conclusion -->
+        <section>
+            <h2 class="text-2xl font-semibold mb-4">Conclusion</h2>
+            <p>
+                In conclusion, the benefits of hiring a professional tile cleaning company far outweigh the DIY alternatives. Their expertise, specialized equipment, time-saving capabilities, and commitment to health and safety make them the ideal choice for maintaining and rejuvenating your tiles.
+            </p>
+            <p>
+                Say goodbye to the frustrations of DIY tile cleaning, and entrust your tile cleaning needs to the professionals. Enjoy the beauty and longevity of your tiles without the hassle.
+            </p>
+        </section>
+    </div>
+    </div>
+    <style>
+          main {
+           
+            margin: 0 auto;
+          }
+          h1 {
+            font-size: 48px;
+            line-height: 48px;
+            text-transform: uppercase;
+            text-align: center;
+            background: var(--dark);
+            color: white;
+            padding: 48px 25%;
+            margin: 0;
+          }
+          main > img:first-of-type {
+            width: 100%;
+            max-width: none;
+          }
+          main img {
+            width: 100%;
+            max-width: 700px;
+            margin: 0 auto;
+            display: block;
+          }
+          main p {
+            font-size: 20px;
+            font-weight: 200;
+            max-width: 700px;
+            margin: 64px auto;
+            padding: 0 16px
+          }
+          main h2 {
+            margin-top: 48px;
+          }
+          main a {
+            margin: 24px auto 64px;
+            display: block;
+            width: fit-content;
+            font-size: 24px;
+            text-decoration: underline;
+          }
+          @media screen and (max-width: 820px) {
+            h1 {
+              padding: 32px 16px;
+              font-size: 32px;
+              line-height: 32px;
+            }
+            main p {
+              margin: 32px auto;
+            }
+            main a {
+              font-size: 20px;
+              text-align: center;
+              margin: 0 auto 64px;
+              max-width: 288px;
+            }
+          }
+        </style>
+        `,
+  }
 ];
